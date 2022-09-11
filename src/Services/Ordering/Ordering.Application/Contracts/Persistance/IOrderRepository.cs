49082cda89a1ce2b83f56041609d0e705d420483
muslumozturk61 <muslum.ozturk@hotmail.com>
2022-09-11 20:27:42 +0300

    order yapısı ayağa kaldırdı

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistance/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Contracts.Persistance
+{
+    public interface IOrderRepository:IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
