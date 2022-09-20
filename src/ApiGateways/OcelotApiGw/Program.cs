34f11e24b99949af3d29d589c194d48092944389
muslumozturk61 <muslum.ozturk@hotmail.com>
2022-09-21 00:22:09 +0300

    gateway katmanları tamamlandı

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true);
+});
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+builder.Services.AddOcelot().AddCacheManager(x =>
+{
+    x.WithDictionaryHandle();
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        public ICatalogService _catalogService;
+        public IBasketService _basketService;
+        public IOrderService _orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        [HttpGet("{userName}", Name = "GetShopping")]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+        {
+            var basket = await _basketService.GetBasket(userName);
+
+            foreach (var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders= await _orderService.GetOrdersByUserName(userName);
+
+            var shoppingModel = new ShoppingModel
+            {
+                UserName = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shoppingModel);
+        }
+    }
+}
